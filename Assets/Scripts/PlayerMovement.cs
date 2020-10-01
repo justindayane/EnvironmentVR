3dd8e5b6d4094e8d02617cf5e381b06afec5cc1c
Hannan Abid <abidh0601@gmail.com>
2020-10-01 13:49:22 -0400

    Fixed build issues and added packages
    Added terrain tools package and samples
    Added standard unity assets
    Fixed error in PlayerMovement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private int playerSpeed;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+            Debug.Log("button clicked");
+        }
+    }
+}
